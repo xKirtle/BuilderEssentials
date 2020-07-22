d525b984a69d1000124c4c98bb0014a1b2d7196a
Kirtle <xkirtle26@gmail.com>
2020-07-22 21:54:23 +0100

    Added a new test command

---
+++ Commands/EventCommand.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace BuilderEssentials.Commands
+{
+    class EventCommand : ModCommand
+    {
+        public override string Command => "event";
+        public override string Description => "Plays a random test event";
+        public override CommandType Type => CommandType.Chat;
+
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            //Empty
+        }
+    }
+}
