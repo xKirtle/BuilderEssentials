a3c91f384fb6f98a851685934c6a5745f1d5785e
xKirtle <xkirtle26@gmail.com>
2020-08-19 00:07:44 +0100

    Removed untested code

--- Items/InfinitePaintBucket.cs
+++ Items/InfinitePaintBucket.cs
@@ -11,10 +11,7 @@ namespace BuilderEssentials.Items
     {
         public override string Texture => "BuilderEssentials/Textures/Items/InfinitePaintBucket";
 
-        public override void SetStaticDefaults()
-        {
-            Tooltip.SetDefault("Necessary to use the Super Painting Tool");
-        }
+        public override void SetStaticDefaults() => Tooltip.SetDefault("Necessary to use the Super Painting Tool");
 
         public override void SetDefaults()
         {
@@ -43,46 +40,46 @@ namespace BuilderEssentials.Items
             recipe.AddRecipe();
         }
 
-        bool paintSelected;
-        public override void UpdateInventory(Player player)
-        {
-            if (paintSelected)
-            {
-                //Works fine until inventory is full, not very good
-                int index = Tools.FindNextEmptyInventorySlot();
-                if (index == -1) index = player.inventory.Length;
-                player.inventory[index].stack = 999;
-                player.inventory[index].paint = 1;
+    //    bool paintSelected;
+    //    public override void UpdateInventory(Player player)
+    //    {
+    //        if (paintSelected)
+    //        {
+    //            //Works fine until inventory is full, not very good
+    //            int index = Tools.FindNextEmptyInventorySlot();
+    //            if (index == -1) index = player.inventory.Length;
+    //            player.inventory[index].stack = 999;
+    //            player.inventory[index].paint = 1;
 
-                Main.NewText(index);
-            }
-        }
+    //            Main.NewText(index);
+    //        }
+    //    }
 
 
-        //TODO: MAKE COLOR SELECTION UI (ALSO REPLACE ALTFUNCTIONUSE WITH UPDATEINVENTORY) AND FINISH THE GLOBAL ITEM USEITEM REPLACEMENT
-        public override bool AltFunctionUse(Player player)
-        {
-            Main.NewText("asd");
-            paintSelected = true;
+    //    //TODO: MAKE COLOR SELECTION UI (ALSO REPLACE ALTFUNCTIONUSE WITH UPDATEINVENTORY) AND FINISH THE GLOBAL ITEM USEITEM REPLACEMENT
+    //    public override bool AltFunctionUse(Player player)
+    //    {
+    //        Main.NewText("asd");
+    //        paintSelected = true;
 
-            return true;
-        }
+    //        return true;
+    //    }
     }
 
-    public class asd : GlobalItem
-    {
-        public override bool UseItem(Item item, Player player)
-        {
-            if (item.type == ItemID.SpectrePaintbrush || item.type == ItemID.SpectrePaintRoller || item.type == ItemID.SpectrePaintScraper)
-            {
-                foreach (Item invItem in player.inventory)
-                {
-                    //if infinitepaintbucket is in inventory and has a color selected, manually paint pointed coords with selected byte color
-                    //if paint is in inventory, manually paint pointed coords with the first paint (lowest inv index)
-                }
-            }
+    //public class asd : GlobalItem
+    //{
+    //    public override bool UseItem(Item item, Player player)
+    //    {
+    //        if (item.type == ItemID.SpectrePaintbrush || item.type == ItemID.SpectrePaintRoller || item.type == ItemID.SpectrePaintScraper)
+    //        {
+    //            foreach (Item invItem in player.inventory)
+    //            {
+    //                //if infinitepaintbucket is in inventory and has a color selected, manually paint pointed coords with selected byte color
+    //                //if paint is in inventory, manually paint pointed coords with the first paint (lowest inv index)
+    //            }
+    //        }
 
-            return base.UseItem(item, player);
-        }
-    }
+    //        return base.UseItem(item, player);
+    //    }
+    //}
 }
