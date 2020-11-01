84a03e72f898f6e171fda607ab99d1f5f4ec7c8f
xKirtle <xkirtle26@gmail.com>
2020-11-01 02:30:21 +0000

    Small UI adjustments
    The whole wheel will now always stay within the screen.

--- UI/UIPanels/MultiWandWheel.cs
+++ UI/UIPanels/MultiWandWheel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using BuilderEssentials.UI.Elements;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.GameContent.UI.Elements;
@@ -8,39 +9,42 @@ using Terraria.UI;
 
 namespace BuilderEssentials.UI.UIPanels
 {
-    public class MultiWandWheel : UIPanel
+    public class MultiWandWheel : CustomUIPanel
     {
-        private const float width = 200f, height = 200f;
+        private const float width = 170f, height = 150;
         private const int elementsCount = 6;
         private UIImageButton[] elements;
         private bool elementHovered;
-        private int selectedIndex;
+        public int selectedIndex;
         private UIText hoverText;
 
-        public MultiWandWheel()
+        public MultiWandWheel(float scale = 1f, float opacity = 1f) : base(scale, opacity)
         {
             Width.Set(width, 0);
             Height.Set(height, 0);
             Left.Set(Main.screenWidth / 2 - width, 0);
             Top.Set(Main.screenHeight / 2 - height, 0);
-            BorderColor = Color.Red;
+            SetPadding(0);
+            BorderColor = Color.Transparent;
             BackgroundColor = Color.Transparent;
 
-            //Initialize textures
+            //Initialize image buttons
             string texturePath = "BuilderEssentials/Textures/UIElements/MultiWandWheel/WandWheel";
             elements = new UIImageButton[elementsCount];
             for (int i = 0; i < elementsCount; i++)
                 elements[i] = new UIImageButton(ModContent.GetTexture(texturePath + i));
 
             //Define our Wheel (circle)
-            int radius = 60;
-            double angle = Math.PI / 3;
+            const int radius = 60;
+            const double angle = Math.PI / 3;
 
             for (int i = 0; i < elementsCount; i++)
             {
                 int index = i;
-                double x = (width / 2 - 35f) + (radius * Math.Cos(angle * (i + 3)));
-                double y = (height / 2 - 35f) - (radius * Math.Sin(angle * (i + 3)));
+                //The magic number 22f is half the width/height of the elements[i] Texture size (44x44 pixels)
+                Vector2 offset = new Vector2(width / 2 - 22f, height / 2 - 22f);
+                double x = offset.X + (radius * Math.Cos(angle * (i + 3)));
+                double y = offset.Y - (radius * Math.Sin(angle * (i + 3)));
                 elements[i].Left.Set((float) x, 0);
                 elements[i].Top.Set((float) y, 0);
                 elements[i].SetVisibility(.75f, .4f);
@@ -90,7 +94,7 @@ namespace BuilderEssentials.UI.UIPanels
                     text = "Places rich mahogany leaves (rich mahogany)";
                     break;
             }
-            
+
             elementHovered = true;
             hoverText = new UIText(text, 1, false);
             hoverText.Left.Set(Main.mouseX + 22 - Left.Pixels, 0);
@@ -106,11 +110,30 @@ namespace BuilderEssentials.UI.UIPanels
 
         public void UpdateHoverText()
         {
+            if (IsMouseHovering)
+                Main.LocalPlayer.mouseInterface = false;
+            
+            if (!Visible) return;
             if (elementHovered)
             {
+                Main.LocalPlayer.mouseInterface = true;
                 hoverText?.Left.Set(Main.mouseX + 22 - Left.Pixels, 0);
                 hoverText?.Top.Set(Main.mouseY + 22 - Top.Pixels, 0);
             }
+        }
+
+        public override void Show()
+        {
+            base.Show();
+
+            //Making sure the UI will stay within our screen
+            float offsetX = Main.mouseX - width / 2 > 0 ? Main.mouseX - width / 2 : 0;
+            offsetX = Main.mouseX + width / 2 > Main.screenWidth ? Main.screenWidth - width : offsetX;
+            float offsetY = Main.mouseY - height / 2 > 0 ? Main.mouseY - height / 2 : 0;
+            offsetY = Main.mouseY + height / 2 > Main.screenHeight ? Main.screenHeight - height : offsetY;
+            
+            Left.Set(offsetX, 0);
+            Top.Set(offsetY, 0);
         }
     }
 }
