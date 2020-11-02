7e56746a1958c73b5d1f2558b9c83db23ec8ce5c
xKirtle <xkirtle26@gmail.com>
2020-11-02 17:03:55 +0000

    Added custom properties to the ModPlayer
    Infinite Range, Infinite Placement and Placement Anywhere.

--- BEPlayer.cs
+++ BEPlayer.cs
@@ -8,19 +8,21 @@ namespace BuilderEssentials
 {
     public class BEPlayer : ModPlayer
     {
-        //TODO: Make properties for infinite range, infinite placement
-        public bool ValidCursorPos => Player.tileTargetX != 0 && Player.tileTargetY != 0;
+        public bool ValidCursorPos => Player.tileTargetX > 0 && Player.tileTargetX < Main.maxTilesX &&
+                                      Player.tileTargetY > 0 && Player.tileTargetY < Main.maxTilesY;
 
-        public Vector2 pointedCoord;
-        public Vector2 pointedTileCoord;
-        public Tile pointedTile;
+        public Vector2 PointedCoord => Main.MouseWorld;
+        public Vector2 PointedTileCoord => new Vector2(Player.tileTargetX, Player.tileTargetY);
+        public Tile PointedTile => Framing.GetTileSafely(PointedTileCoord);
+        public bool InfiniteRange { get; set; }
+        public bool InfinitePlacement { get; set; }
+        public bool PlacementAnywhere { get; set; }
+
         public override void PreUpdate()
         {
             if (Main.netMode != NetmodeID.Server && ValidCursorPos)
             {
-                pointedCoord = Main.MouseWorld;
-                pointedTileCoord = new Vector2(Player.tileTargetX, Player.tileTargetY);
-                pointedTile = Framing.GetTileSafely(pointedTileCoord);
+                
             }
         }
     }
