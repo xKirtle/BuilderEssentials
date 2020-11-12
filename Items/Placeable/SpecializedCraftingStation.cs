ca10fc2a5cc65346dfa81ed28dab7ec180c65a71
xKirtle <xkirtle26@gmail.com>
2020-11-12 15:53:57 +0000

    Added the SpecializedCraftingStation

---
+++ Items/Placeable/SpecializedCraftingStation.cs
@@ -0,0 +1,41 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace BuilderEssentials.Items.Placeable
+{
+    class SpecializedCraftingStation : ModItem
+    {
+        public override string Texture => "BuilderEssentials/Textures/Items/Placeable/SpecializedCraftingStation";
+        public override void SetStaticDefaults() => Tooltip.SetDefault("Used to craft Specialized Recipes");
+
+        public override void SetDefaults()
+        {
+            item.width = 64;
+            item.height = 50;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.value = 150;
+            item.createTile = TileType<Tiles.SpecializedCraftingStation>();
+            item.rare = ItemRarityID.Red;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Keg);
+            recipe.AddIngredient(ItemID.BlendOMatic);
+            recipe.AddIngredient(ItemID.MeatGrinder);
+            // recipe.AddRecipeGroup("BuilderEssentials:Campfires");
+
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
