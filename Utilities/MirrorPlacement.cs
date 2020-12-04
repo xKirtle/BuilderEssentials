8605d608dc352874ad8f37e33c914c84e92cc8e7
xKirtle <xkirtle26@gmail.com>
2020-12-04 02:08:17 +0000

    Finished MirrorPlacement
    A bit untested but hopefully it's okay. Will test more throughoutly later

--- Utilities/MirrorPlacement.cs
+++ Utilities/MirrorPlacement.cs
@@ -15,34 +15,52 @@ namespace BuilderEssentials.Utilities
         {
             MirrorWandSelection ms = UIStateLogic1.mirrorWandSelection;
             CoordsSelection cs = ms.cs;
-            
+
             //Mirror coords
             Vector2 start = cs.LMBStart;
             Vector2 end = cs.LMBEnd;
-            
+
+            //Check if coords are within selection
             if (!ms.validMirrorPlacement || !HelperMethods.IsWithinRange(i, cs.RMBStart.X, cs.RMBEnd.X) ||
                 !HelperMethods.IsWithinRange(j, cs.RMBStart.Y, cs.RMBEnd.Y)) return;
 
-            
+            //Check if coords intersect the mirror axis
+            if (!HelperMethods.IsWithinRange(i, start.X, end.X, true) &&
+                !HelperMethods.IsWithinRange(j, start.Y, end.Y, true)) return;
+
+
             //Compensate for vanilla's weird tile origin
             int correctionOrigin = 0;
-            Point16 tileOrigin = Point16.Zero;
-            int tileSize = 0;
+            Point16 tileOrigin;
+            int tileSize;
             
-            //Know beforehand what is a multitile and what is not
             if (WhatIsThisItem(itemType) == ItemTypes.Tile)
             {
                 Tile tile = Framing.GetTileSafely(i, j);
-                tileOrigin = TileObjectData.GetTileData(tile).Origin;
-                tileSize = TileObjectData.GetTileData(tile).CoordinateFullWidth / 16;
+                TileObjectData data = TileObjectData.GetTileData(tile);
+
+                if (data != null)
+                {
+                    tileOrigin = TileObjectData.GetTileData(tile).Origin;
+                    tileSize = TileObjectData.GetTileData(tile).CoordinateFullWidth / 16;
+
+                    if (tileSize == 2 && (tileOrigin == new Point16(0, 0) || tileOrigin == new Point16(0, 1) ||
+                        tileOrigin == new Point16(0, 2)) || tileOrigin == new Point16(0, 4) || tileOrigin == new Point16(1, 2))
+                        correctionOrigin -= 1;
+
+                    if (tileSize == 2 && tileOrigin == new Point16(1, 1))
+                        correctionOrigin += 1;
+
+                    if (tileSize == 4 && (tileOrigin == new Point16(1, 1) || tileOrigin == new Point16(1, 3)))
+                        correctionOrigin -= 1;
+
+                    if (tileSize == 4 && tileOrigin == new Point16(1, 2))
+                        correctionOrigin -= 2;
+                }
             }
-            
-            
-            
             
             //0:TopLeft; 1:TopRight; 2:BottomLeft; 3:BottomRight;
             bool LeftRight = ms.selectedQuarter == 1 || ms.selectedQuarter == 3;
-            bool RightLeft = ms.selectedQuarter == 0 || ms.selectedQuarter == 2;
             bool TopBottom = ms.selectedQuarter == 2 || ms.selectedQuarter == 3;
             bool BottomTop = ms.selectedQuarter == 0 || ms.selectedQuarter == 1;
 
@@ -51,7 +69,7 @@ namespace BuilderEssentials.Utilities
                 float distanceToMirror = start.X - i > end.X - i ? start.X - i : end.X - i;
 
                 float newPosX;
-                newPosX = end.X; /* + correction*/
+                newPosX = end.X + correctionOrigin;
                 newPosX += distanceToMirror < 0 ? -Math.Abs(distanceToMirror) : Math.Abs(distanceToMirror);
                 if (ms.wideMirror && LeftRight) newPosX -= 1;
                 if (HelperMethods.IsWithinRange(newPosX, cs.RMBStart.X, cs.RMBEnd.X))
