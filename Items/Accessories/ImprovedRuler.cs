318352c72319ef8bf3b73201c0a2f854845c1851
xKirtle <xkirtle26@gmail.com>
2020-12-18 14:03:20 +0000

    Added the ImprovedRuler assets

---
+++ Items/Accessories/ImprovedRuler.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BuilderEssentials.Items.Accessories
+{
+    public class ImprovedRuler : ModItem
+    {
+        public override string Texture => "BuilderEssentials/Textures/Items/Accessories/ImprovedRuler";
+        public override void SetDefaults()
+        {
+            item.accessory = true;
+            item.vanity = false;
+            item.width = 42;
+            item.height = 42;
+            item.value = Item.sellPrice(0, 10, 0, 0);
+            item.rare = ItemRarityID.Red;
+        }
+    }
+}
