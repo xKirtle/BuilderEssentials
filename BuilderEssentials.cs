5d7f05f5d6d860a6f42c282b0a9e45073c9fcf2a
xKirtle <xkirtle26@gmail.com>
2021-08-06 14:00:57 +0100

    Utilities

--- BuilderEssentials.cs
+++ BuilderEssentials.cs
@@ -1,8 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BuilderEssentials.UI.UIStates;
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace BuilderEssentials
 {
 	public class BuilderEssentials : Mod
 	{
+		public override void Load()
+		{
+			//hotkeys init
+		}
+
+		public override void Unload()
+		{
+			
+		}
 	}
 }
