faf2f9e669d76a63fd886474154bccef5e3aac50
xKirtle <xkirtle26@gmail.com>
2021-08-07 18:00:15 +0100

    Quadratic and Cubic Bezier Curves

---
+++ UI/Elements/ShapesDrawer/BezierCurve.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using BuilderEssentials.UI.UIStates;
+using BuilderEssentials.Utilities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ID;
+
+namespace BuilderEssentials.UI.Elements.ShapesDrawer
+{
+    internal class BezierCurve : BaseShape
+    {
+        public CoordsSelection cs;
+        public void DrawQuadraticBezier(float dt, Vector2 startPoint, Vector2 controlPoint, Vector2 endPoint)
+        {
+            List<Vector2> points = new List<Vector2>();
+            for (float t = 0.0f; t < 1.0f; t += dt)
+                points.Add(HelperMethods.TraverseBezier(startPoint, controlPoint, endPoint, t));
+
+            points.Add(HelperMethods.TraverseBezier(startPoint, controlPoint, endPoint, 1.0f));
+
+            for (int i = 0; i < points.Count - 1; i++)
+                PlotLine((int)points[i].X, (int)points[i].Y, (int)points[i+1].X, (int)points[i+1].Y);
+        }
+
+        public void DrawCubicBezier(float dt, Vector2 startPoint, Vector2 controlPoint, Vector2 controlPoint2, Vector2 endPoint)
+        {
+            List<Vector2> points = new List<Vector2>();
+            for (float t = 0.0f; t < 1.0; t += dt)
+                points.Add(HelperMethods.TraverseBezier(startPoint, controlPoint, controlPoint2, endPoint, t));
+            
+            points.Add(HelperMethods.TraverseBezier(startPoint, controlPoint, controlPoint2, endPoint, 1.0f));
+
+            for (int i = 0; i < points.Capacity - 1; i++)
+                PlotLine((int)points[i].X, (int)points[i].Y, (int)points[i+1].X, (int)points[i+1].Y);
+        }
+
+        public BezierCurve()
+        {
+            cs = new CoordsSelection(ItemID.None, BaseUIState.Instance);
+        }
+        
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            //Prevents drawing and coord updates if Improved Ruler is not equipped
+           // if (!Main.LocalPlayer.GetModPlayer<BEPlayer>().improvedRulerEquipped) return;
+            
+            cs.UpdateCoords(true);
+            color = Blue;
+            
+            if (cs.LMBStart != cs.LMBEnd) //&& ImprovedRuler is equipped
+                DrawQuadraticBezier(0.5f, cs.LMBStart, cs.RMBEnd, cs.LMBEnd);
+        }
+    }
+}
---
+++ Utilities/HelperMethods.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace BuilderEssentials.Utilities
+{
+    internal static partial class HelperMethods
+    {
+        //Quadratic bezier
+        internal static float X(float t, float x0, float x1, float x2)
+        {
+            return (float) (
+                x0 * Math.Pow(1 - t, 2) +
+                x1 * 2 * t * (1 - t) +
+                x2 * Math.Pow(t, 2)
+            );
+        }
+
+        internal static float Y(float t, float y0, float y1, float y2)
+        {
+            return (float) (
+                y0 * Math.Pow(1 - t, 2) +
+                y1 * 2 * t * (1 - t) +
+                y2 * Math.Pow(t, 2)
+            );
+        }
+
+        internal static Vector2 TraverseBezier(Vector2 startPoint, Vector2 controlPoint, Vector2 endPoint, float t)
+        {
+            float x = X(t, startPoint.X, controlPoint.X, endPoint.X);
+            float y = Y(t, startPoint.Y, controlPoint.Y, endPoint.Y);
+            return new Vector2(x, y);
+        }
+        
+        //Cubic bezier
+        internal static float X(float t, float x0, float x1, float x2, float x3)
+        {
+            return (float)(
+                x0 * Math.Pow((1 - t), 3) +
+                x1 * 3 * t * Math.Pow((1 - t), 2) +
+                x2 * 3 * Math.Pow(t, 2) * (1 - t) +
+                x3 * Math.Pow(t, 3)
+            );
+        }
+        internal static float Y(float t, float y0, float y1, float y2, float y3)
+        {
+            return (float)(
+                y0 * Math.Pow((1 - t), 3) +
+                y1 * 3 * t * Math.Pow((1 - t), 2) +
+                y2 * 3 * Math.Pow(t, 2) * (1 - t) +
+                y3 * Math.Pow(t, 3)
+            );
+        }
+        
+        internal static Vector2 TraverseBezier(Vector2 startPoint, Vector2 controlPoint, Vector2 controlPoint2, Vector2 endPoint, float t)
+        {
+            float x = X(t, startPoint.X, controlPoint.X, controlPoint2.X, endPoint.X);
+            float y = Y(t, startPoint.Y, controlPoint.Y, controlPoint2.Y, endPoint.Y);
+            return new Vector2(x, y);
+        }
+    }
+}
