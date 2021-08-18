ea5a2784db4712ad29505b092c1ec3ab8e6e59ba
xKirtle <xkirtle26@gmail.com>
2021-08-18 21:25:33 +0100

    MultiWand UI and Item

---
+++ Items/MultiWand.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameInput;
+using Terraria.DataStructures;
+using BuilderEssentials.Utilities;
+using BuilderEssentials.UI.UIPanels;
+using BuilderEssentials.UI.UIStates;
+
+namespace BuilderEssentials.Items
+{
+    internal class MultiWand : ModItem
+    {
+        private Vector2 toolRange;
+        private bool canPlaceItems;
+        private MultiWandWheel panel;
+
+        public static int[] wandTypes =
+        {
+            ItemID.LivingWoodWand, 
+            ItemID.BoneWand, 
+            ItemID.LeafWand, 
+            ItemID.HiveWand, 
+            ItemID.LivingMahoganyWand, 
+            ItemID.LivingMahoganyLeafWand
+        };
+        
+        public static int[] wandMaterials =
+        {
+            ItemID.Wood,
+            ItemID.Bone,
+            ItemID.Wood,
+            ItemID.Hive,
+            ItemID.RichMahogany,
+            ItemID.RichMahogany
+        };
+
+        public static int[] wandPlacedTiles =
+        {
+            TileID.LivingWood,
+            TileID.BoneBlock,
+            TileID.LeafBlock,
+            TileID.Hive,
+            TileID.LivingMahogany,
+            TileID.LivingMahoganyLeaves
+        };
+
+        public override string Texture => "BuilderEssentials/Textures/Items/MultiWand";
+
+        public override void SetStaticDefaults() =>
+            Tooltip.SetDefault("Contains all building wands!\nRight Click to open selection menu");
+
+        public override void SetDefaults()
+        {
+            Item.height = 44;
+            Item.width = 44;
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.value = Item.buyPrice(0, 2, 0, 0);
+            Item.rare = ItemRarityID.Red;
+            Item.autoReuse = true;
+            Item.noMelee = false;
+            toolRange = new Vector2(9, 8);
+            
+            panel = UIUIState.Instance.multiWandWheel;
+        }
+
+        public override Vector2? HoldoutOffset() => new Vector2(2, -9);
+
+        public override void HoldItem(Player player)
+        {
+            if (player.whoAmI != Main.myPlayer) return;
+
+            BEPlayer mp = player.GetModPlayer<BEPlayer>();
+            if (Main.netMode != NetmodeID.Server && mp.ValidCursorPos)
+            {
+                canPlaceItems = HelperMethods.ToolHasRange(toolRange) && !Main.LocalPlayer.mouseInterface;
+                player.cursorItemIconEnabled = canPlaceItems;
+                player.cursorItemIconID = Item.type;
+            }
+        }
+
+        private bool CanReduceWandAmmo(int amount = 1, bool reduceStack = true, bool itemShouldBeInHand = false)
+        {
+            int materialType = wandMaterials[panel.selectedIndex];
+            return HelperMethods.CanReduceItemStack(materialType);
+        }
+        
+        public override bool CanUseItem(Player player)
+        {
+            if (player.whoAmI != Main.myPlayer || !canPlaceItems) return false;
+            
+            int tileType = wandPlacedTiles[panel.selectedIndex];
+
+            if (HelperMethods.ValidTilePlacement(Player.tileTargetX, Player.tileTargetY) && CanReduceWandAmmo(reduceStack: false))
+            {
+                bool forced = HelperMethods.CanBreakToPlaceTile(Player.tileTargetX, Player.tileTargetY);
+                WorldGen.PlaceTile(Player.tileTargetX, Player.tileTargetY, tileType, forced: forced);
+
+                if (Main.netMode == NetmodeID.MultiplayerClient)
+                    NetMessage.SendTileSquare(-1, Player.tileTargetX, Player.tileTargetY, 1);
+            }
+
+            return true;
+        }
+
+        private int mouseRightTimer = 0;
+
+        public override void UpdateInventory(Player player)
+        {
+            if (player.whoAmI != Main.myPlayer) return;
+
+            if (Main.mouseRight && player.HeldItem == Item &&
+                (HelperMethods.IsUIAvailable() || panel.IsMouseHovering) && ++mouseRightTimer == 2)
+                UIUIState.Instance.multiWandWheel.Toggle();
+
+            if (Main.mouseRightRelease)
+                mouseRightTimer = 0;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+            .AddIngredient(ItemID.LivingWoodWand)
+            .AddIngredient(ItemID.BoneWand)
+            .AddIngredient(ItemID.LeafWand)
+            .AddIngredient(ItemID.HiveWand)
+            .AddIngredient(ItemID.LivingMahoganyWand)
+            .AddIngredient(ItemID.LivingMahoganyLeafWand)
+            .AddTile(TileID.TinkerersWorkbench)
+            .Register();
+        }
+    }
+}
