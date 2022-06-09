d465965225d1b709008a15f8b9ef8497f6a1a029
Kirtle <xkirtle26@gmail.com>
2022-06-09 14:34:30 +0100

    To be implemented Infinite items solution

---
+++ Common/GlobalItems/InfiniteGlobalItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace BuilderEssentials.Common.GlobalItems;
+
+public class InfiniteGlobalItem : GlobalItem
+{
+    public override bool ConsumeItem(Item item, Player player) {
+        //Kirtle: Return false for infinite items! Works for tile wands as well
+        
+        return base.ConsumeItem(item, player);
+    }
+}
