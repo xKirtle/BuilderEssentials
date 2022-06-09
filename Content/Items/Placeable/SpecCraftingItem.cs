6c0e9265b26f9422c003d699fc752161454a65b5
Kirtle <xkirtle26@gmail.com>
2022-06-09 19:50:09 +0100

    Specialized Crafting Station Tile + Item

---
+++ Content/Items/Placeable/SpecCraftingItem.cs
@@ -0,0 +1,35 @@
+﻿using BuilderEssentials.Content.Tiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BuilderEssentials.Content.Items.Placeable;
+
+public class SpecCraftingItem : ModItem
+{
+    public override string Texture => "BuilderEssentials/Assets/Items/Placeable/" + GetType().Name;
+
+    public override void SetStaticDefaults() {
+        DisplayName.SetDefault("Specialized Crafting Station");
+        Tooltip.SetDefault("Used to craft Specialized items");
+    }
+
+    public override void SetDefaults() {
+        Item.width = 64;
+        Item.height = 50;
+        Item.maxStack = 99;
+        Item.useTurn = true;
+        Item.autoReuse = true;
+        Item.useAnimation = 15;
+        Item.useTime = 10;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.consumable = true;
+        Item.value = Item.sellPrice(gold: 7);
+        Item.createTile = ModContent.TileType<SpecCraftingStation>();
+        Item.rare = ItemRarityID.Red;
+    }
+
+    public override void AddRecipes() {
+        
+    }
+}
---
+++ Content/Tiles/SpecCraftingStation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BuilderEssentials.Content.Items.Placeable;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace BuilderEssentials.Content.Tiles;
+
+[Autoload(true)]
+public class SpecCraftingStation : BaseCraftingStation
+{
+    public override string DisplayName =>"Specialized Crafting Station";
+    public override Color MapColor => new Color(39, 137, 205);
+
+    public override void SetTileObjectData() {
+        TileObjectData.newTile.UsesCustomCanPlace = true;
+        TileObjectData.newTile.Origin = new Point16(1, 2);
+        TileObjectData.newTile.CoordinatePadding = 2;
+        TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
+        TileObjectData.newTile.Height = 3;
+        TileObjectData.newTile.CoordinateWidth = 16;
+        TileObjectData.newTile.Width = 4;
+        TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop, TileObjectData.newTile.Width, 0);
+        TileObjectData.addTile(Type);
+    }
+
+    public override int[] AdjacentTiles() => new int[] 
+        { TileID.Campfire, TileID.Kegs, TileID.Blendomatic, TileID.MeatGrinder };
+
+    
+    public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+        Item.NewItem(null, i * 16, j * 16, 32, 16, ModContent.ItemType<SpecCraftingItem>());
+    }
+}
