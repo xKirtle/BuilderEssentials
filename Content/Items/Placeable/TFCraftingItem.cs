966a133f227f745b8fb03af5c01004ae60855f7c
Kirtle <xkirtle26@gmail.com>
2022-06-09 19:50:25 +0100

    Themed Furniture Crafting Station

---
+++ Content/Items/Placeable/TFCraftingItem.cs
@@ -0,0 +1,35 @@
+﻿using BuilderEssentials.Content.Tiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BuilderEssentials.Content.Items.Placeable;
+
+public class TFCraftingItem : ModItem
+{
+    public override string Texture => "BuilderEssentials/Assets/Items/Placeable/" + GetType().Name;
+
+    public override void SetStaticDefaults() {
+        DisplayName.SetDefault("Themed Furniture Crafting Station");
+        Tooltip.SetDefault("Used to craft Themed Furniture items");
+    }
+
+    public override void SetDefaults() {
+        Item.width = 64;
+        Item.height = 64;
+        Item.maxStack = 99;
+        Item.useTurn = true;
+        Item.autoReuse = true;
+        Item.useAnimation = 15;
+        Item.useTime = 10;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.consumable = true;
+        Item.value = Item.sellPrice(gold: 25);
+        Item.createTile = ModContent.TileType<TFCraftingStation>();
+        Item.rare = ItemRarityID.Red;
+    }
+
+    public override void AddRecipes() {
+        
+    }
+}
---
+++ Content/Tiles/TFCraftingStation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BuilderEssentials.Content.Items.Placeable;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace BuilderEssentials.Content.Tiles;
+
+[Autoload(true)]
+public class TFCraftingStation : BaseCraftingStation
+{
+    public override string DisplayName =>"Themed Furniture Crafting Station";
+    public override Color MapColor => new Color(185, 92, 31);
+
+    public override void SetTileObjectData() {
+        TileObjectData.newTile.UsesCustomCanPlace = true;
+        TileObjectData.newTile.Origin = new Point16(1, 3);
+        TileObjectData.newTile.CoordinatePadding = 2;
+        TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 16};
+        TileObjectData.newTile.Height = 4;
+        TileObjectData.newTile.CoordinateWidth = 16;
+        TileObjectData.newTile.Width = 4;
+        TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop, TileObjectData.newTile.Width, 0);
+        TileObjectData.addTile(Type);
+    }
+    
+    public override int[] AdjacentTiles() => new int[] { TileID.BoneWelder, TileID.GlassKiln, TileID.HoneyDispenser, 
+        TileID.IceMachine, TileID.LivingLoom, TileID.SkyMill, TileID.Solidifier, TileID.FleshCloningVat, 
+        TileID.SteampunkBoiler, TileID.LihzahrdFurnace, TileID.WaterDrip, TileID.Waterfall, TileID.LavaDrip, 
+        TileID.Lavafall, TileID.HoneyDrip, TileID.Honeyfall, TileID.SnowCloud };
+
+    
+    public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+        Item.NewItem(null, i * 16, j * 16, 32, 16, ModContent.ItemType<TFCraftingItem>());
+    }
+}
