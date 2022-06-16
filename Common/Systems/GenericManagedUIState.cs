e1f40f19bed7876521907c2bf58502e8efd0a751
Kirtle <xkirtle26@gmail.com>
2022-06-16 17:39:41 +0100

    ManagedUIState moved to Common for consistency

--- Common/Systems/GenericManagedUIState.cs
+++ Common/Systems/GenericManagedUIState.cs
@@ -4,19 +4,19 @@ using BuilderEssentials.Common;
 using Terraria.ModLoader;
 using Terraria.UI;
 
-namespace BuilderEssentials.Content.UI;
+namespace BuilderEssentials.Common.Systems;
 
 //T is the base UIElement type for the UIState
 public abstract class ManagedUIState<T> : UIState, IDisposable where T : UIElement
 {
     private static ManagedUIState<T> instance;
     private List<T> uiStateElements = new();
-    public virtual List<Type> PanelTypes { get; protected set; } = new();
+    public abstract List<Type> PanelTypes();
     
     public override void OnInitialize() {
         instance = this;
-        for (var i = 0; i < PanelTypes.Count; i++)
-            uiStateElements.Add((T)Activator.CreateInstance(PanelTypes[i]));
+        for (var i = 0; i < PanelTypes().Count; i++)
+            uiStateElements.Add((T)Activator.CreateInstance(PanelTypes()[i]));
     }
 
     public virtual void Dispose() {
@@ -26,14 +26,13 @@ public abstract class ManagedUIState<T> : UIState, IDisposable where T : UIEleme
         }
 
         uiStateElements = null;
-        PanelTypes = null;
         instance = null;
     }
 
     public static ManagedUIState<T> GetInstance() => instance;
 
     private static int IndexOfPanelType<U>()
-        => GetInstance().PanelTypes.IndexOf(typeof(U));
+        => GetInstance().PanelTypes().IndexOf(typeof(U));
     
     private static T GetElementAtIndex(int index) => (T) GetInstance().uiStateElements[index];
 
