a983eb16f56f48a141c952f2e4a8bf32a21195ac
Kirtle <xkirtle26@gmail.com>
2022-06-20 16:15:12 +0100

    PlacementHistory

---
+++ Common/PlacementHistory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace BuilderEssentials.Common;
+
+public struct PlacementHistory
+{
+    public Point Coordinate;
+    public MinimalTile PreviousTile;
+    public MinimalTile PlacedTile;
+
+    public PlacementHistory(Point coordinate, MinimalTile previousTile, MinimalTile placedTile) {
+        Coordinate = coordinate;
+        PreviousTile = previousTile;
+        PlacedTile = placedTile;
+    }
+}
+
+public struct MinimalTile
+{
+    public int TileType;
+    public int WallType;
+    public bool HasTile;
+    public bool IsWall { get; }
+
+    public MinimalTile(int tileType, int wallType, bool hasTile) {
+        TileType = tileType;
+        WallType = wallType;
+        HasTile = hasTile;
+        IsWall = WallType > 0 && !HasTile && TileType <= 0;
+    }
+
+    public override string ToString() {
+        return $"TileType: {TileType} WallType: {WallType} HasTile: {HasTile} IsWall: {IsWall}";
+    }
+}
