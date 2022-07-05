080be6e3e98405a76cbc0cddf48bed6745b89bd7
Kirtle <xkirtle26@gmail.com>
2022-07-05 12:28:57 +0100

    Dynamic wrench upgrade mod item

---
+++ Content/Items/WrenchItemUpgrade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BuilderEssentials.Common.Enums;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace BuilderEssentials.Content.Items;
+
+[Autoload(false)]
+public class WrenchItemUpgrade : ModItem
+{
+    public int UpgradeNumber { get; set; }
+    string ItemTexture = "BuilderEssentials/Assets/Items/WrenchUpgrades/Upgrade";
+    public override string Texture => ItemTexture + UpgradeNumber;
+    protected override bool CloneNewInstances => true;
+
+    //Can't have repeated internal names when adding content
+    public override string Name => ((WrenchUpgrades) UpgradeNumber).ToString() + "Module";
+
+    public override void SetDefaults() {
+        Item.width = Item.height = 38;
+        Item.value = Item.sellPrice(0);
+        Item.rare = ItemRarityID.Red;
+    }
+
+    public WrenchItemUpgrade() { }
+    public WrenchItemUpgrade(int upgradeNumber) => UpgradeNumber = upgradeNumber;
+
+    public override ModItem Clone(Item newEntity) {
+        WrenchItemUpgrade newInstance = (WrenchItemUpgrade) base.Clone(newEntity);
+        newInstance.UpgradeNumber = UpgradeNumber;
+        return newInstance;
+    }
+}
